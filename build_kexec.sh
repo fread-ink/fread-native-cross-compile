045d64fe425020d64788cec4d91848d8bce7231c
Marc Juul <github@juul.io>
2016-05-15 08:44:14 -0700

    starting writing build_kexec.sh

---
+++ build_kexec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+wget https://www.kernel.org/pub/linux/utils/kernel/kexec/kexec-tools-2.0.12.tar.gz
+tar xvzf kexec-tools-2.0.12.tar.gz
+cd kexec-tools-2.0.12/
+
+echo "TODO: Actually compile stuff."
+
+set +e
